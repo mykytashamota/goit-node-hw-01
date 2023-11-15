85c977908573866084b922f127f9b9f1984bf1c2
mykytashamota <nik.shamota@gmail.com>
2023-11-15 14:00:17 +0200

    added functions for working with contacts

---
+++ db/contacts.js
@@ -0,0 +1,46 @@
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
+
+const contactsPath = path.join(__dirname, "./contacts.json");
+
+const getContacts = async () => {
+  const contacts = await fs.readFile(contactsPath);
+  console.log(contacts);
+  return JSON.parse(contacts);
+};
+
+const getContactById = async (id) => {
+  const contacts = await getContacts();
+  const contact = contacts.find((contact) => contact.id === id);
+  return contact || null;
+};
+
+const addContact = async (data) => {
+  const contacts = await getContacts();
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  };
+  contacts.push(newContact);
+  fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContact;
+};
+
+const removeContact = async (id) => {
+  const contacts = await getContacts();
+  const index = contacts.findIndex((contact) => contact.id === id);
+  if (index !== -1) {
+    return null;
+  }
+  const [removedContact] = contacts.splice(index, 1);
+  fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return removedContact;
+};
+
+module.exports = {
+  getContacts,
+  getContactById,
+  addContact,
+  removeContact,
+};
